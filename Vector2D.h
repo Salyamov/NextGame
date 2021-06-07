911572fc96931989696d21d52b08ba92474bad5a
bmpcool@yandex.ru <bmpcool@yandex.ru>
2021-06-08 02:07:55 +0500

    1

--- Vector2D.h
+++ Vector2D.h
@@ -24,6 +24,18 @@ public:
 		return v1;
 	}
 
+	Vector2D operator - (const Vector2D& v2) const
+	{
+		return Vector2D(m_x - v2.m_x, m_y - v2.m_y);
+	}
+
+	friend Vector2D& operator -= (Vector2D& v1, Vector2D& v2)
+	{
+		v1.m_x -= v2.m_x;
+		v1.m_y -= v2.m_y;
+		return v1;
+	}
+
 	Vector2D operator * (float scalar)
 	{
 		return Vector2D(m_x * scalar, m_y * scalar);
@@ -36,6 +48,17 @@ public:
 		return *this;
 	}
 
+	Vector2D operator / (float scalar)
+	{
+		return Vector2D(m_x / scalar, m_y / scalar);
+	}
+
+	Vector2D& operator /= (float scalar)
+	{
+		m_x /= scalar;
+		m_y /= scalar;
+		return *this;
+	}
 
 
 private:
