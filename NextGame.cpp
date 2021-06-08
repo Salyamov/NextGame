4b6231a14d7b399638f2ce1c9a5432f132efdc7a
bmpcool@yandex.ru <bmpcool@yandex.ru>
2021-06-08 22:43:12 +0500

    1

--- NextGame.cpp
+++ NextGame.cpp
@@ -3,26 +3,36 @@
 #include <Windows.h>
 #include <iostream>
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.f / FPS;
 
 Game* g_game = NULL;
 
 int main(int argc, char* args[])
 {
-
 	AllocConsole();
 	FILE* file;
 	file = freopen("CON", "w", stdout);
+
+	Uint32 frameStart, frameTime;
 
 	if (TheGame::Instance()->init("NextGame", 100, 100, 640, 480, false))
 	{
 		std::cout << "game init complete\n";
 		while (TheGame::Instance()->running())
 		{
+			frameStart = SDL_GetTicks();
+
 			TheGame::Instance()->handleEvents();
 			TheGame::Instance()->update();
 			TheGame::Instance()->render();
 
-			SDL_Delay(20);
+			frameTime = SDL_GetTicks() - frameStart; //сколько понадобилось времени на одну прорисовку
+
+			if (frameTime < DELAY_TIME)
+			{
+				SDL_Delay((int)DELAY_TIME - frameTime);
+			}
 		}
 	}
 	else
