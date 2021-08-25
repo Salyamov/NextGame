1625ca4e0a4f8332f7a36dfbc1c876475b31a9e3
bmpcool@yandex.ru <bmpcool@yandex.ru>
2021-08-25 13:00:52 +0500

    1

---
+++ BulletHandler.h
@@ -0,0 +1,71 @@
+#ifndef BULLETHANDLER_H
+#define BULLETHANDLER_H
+#include <vector>
+#include "Bullet.h"
+#include "Vector2D.h"
+
+class BulletHandler
+{
+public:
+
+	void addPlayerBullet(int x, int y, int width, int height, std::string textureID, int numFrames, Vector2D heading);
+	void addEnemyBullet(int x, int y, int width, int height, std::string textureID, int numFrames, Vector2D heading);
+
+	void updateBullets();
+	void drawBullets();
+	
+	void clearBullets();
+
+	const std::vector<PlayerBullet*> getPlayerBullets() { return m_playerBullets; }
+	const std::vector<EnemyBullet*>  getEnemyBullets() { return m_enemyBullets; }
+
+	static BulletHandler* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new BulletHandler;
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+
+private:
+	
+	static BulletHandler* s_pInstance;
+	BulletHandler();
+	~BulletHandler();
+
+	std::vector<PlayerBullet*> m_playerBullets;
+	std::vector<EnemyBullet*> m_enemyBullets;
+
+	//BulletHandler(const BulletHandler&);
+	//BulletHandler& operator=(const BulletHandler&);
+
+
+};
+
+typedef BulletHandler TheBulletHandler;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
